ac4f246c74a8bc3329044211fccacf601f6620b9
bsuttonwills <bsuttonwills@csumb.edu>
2020-09-21 01:31:08 -0700

    Part 2 of Flixster App Assignment

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Blayne Suttonwills on 9/21/20.
+//  Copyright © 2020 bsuttonwills. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
